02e2028a9a03ac8e8d5be538f43503e89e992da2
Aaron Marriner <amarriner@amarriner.com>
2014-08-19 18:11:45 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Robin (aka Holy ____ Batman!)
+
+*Script to build "Holy ____ Batman!" tweets*
---
+++ robin.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""Script to build Holy ______ Batman! tweets"""
+
+from bs4 import BeautifulSoup
+
+import sys
+
+
+def main():
+   """Initial entry point"""
+   print 'TEST'
+
+
+if __name__ == '__main__':
+   sys.exit(main())
