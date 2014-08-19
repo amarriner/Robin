a8d7aa2ef7e20d68ff906d46e8afcc93aa96704d
Aaron Marriner <amarriner@amarriner.com>
2014-08-19 18:39:09 +0000

    Additional formatting for idioms and general cleanup

--- robin.py
+++ robin.py
@@ -18,14 +18,18 @@ IDIOM_BASE = 'http://www.idiomconnection.com'
 def format_idiom(s):
    """Returns idiom lowercase without parentheticals, punctuation and removing preceeding 'A ...' """
 
+   # Strip leading 'A ' if it exists
    if s[:2].upper() == 'A ':
       s = s[2:]
 
+   # Remove anything inside parentheses
    parenthetical = re.compile(r'\(.*?\)')
    s = re.sub(parenthetical, '', s)
 
+   # Remove most punctuation
    punctuation = re.compile(r'[^A-Za-z0-9 -]')
    s = re.sub(punctuation, '', s)
+
 
    return string.strip(s).lower()
 
@@ -33,6 +37,7 @@ def format_idiom(s):
 def main():
    """Initial entry point"""
 
+   # Retrieve main idiom page
    request = requests.get(IDIOM_BASE)
    soup = BeautifulSoup(request.content)
 
@@ -41,12 +46,14 @@ def main():
    link = random.choice(random.choice(soup.find_all('ul', limit=2)).find_all('a'))
    print link.get('href')
 
+   # Get the randomly picked page with a list of idioims
    request = requests.get(link.get('href'))
    soup = BeautifulSoup(request.content)
 
-   idiom = random.choice(soup.find_all('b')).contents[0]
-   print idiom
-   print format_idiom(idiom)
+   # Take one idiom from that page, and format it
+   idiom = format_idiom(random.choice(soup.find_all('b')).contents[0])
+
+   print 'Holy ' + idiom + ' Batman!'
 
 
 if __name__ == '__main__':
